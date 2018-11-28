2b26ce4390f61408789e9610f8bb7f99e7a2307c
daman85 <44984587+daman85@users.noreply.github.com>
2018-11-28 16:54:29 -0500

    1.0

---
+++ FolderSizePS.ps1
@@ -0,0 +1,23 @@
+﻿
+Param(
+[Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+$Path = ".\"
+)
+
+Process{
+
+$folderList = Get-ChildItem -Path $path -Recurse -Directory |  Select-Object FullName
+
+foreach ($P in $folderList) {
+$Sum = Get-ChildItem $P.FullName -Recurse -Force -File -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum | Select-Object Sum
+
+
+[PSCustomObject]@{
+      "Path" = $P.FullName
+      "Size($Units)" = [math]::Round(($Sum.Sum /1MB),2)
+      }
+
+    }
+    
+}
+
